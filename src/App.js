f18a16c2888ed3680d60f3bf896f56928bdd1166
Andry <andrycalnicow@gmail.com>
2020-01-17 22:18:11 +0400

    style: complete Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
